542b51fe6e3a39046f5b10e26a8de0856aef9871
TheCrazyWolf <thecrazywolf63@gmail.com>
2025-01-26 11:29:26 +0400

    Добавлены файлфы проекта

---
+++ ApiMyPet.Context/PetContext.cs
@@ -0,0 +1,11 @@
+﻿using Api.MyPetAr.Models;
+using Api.MyPetAr.Models.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiMyPet.Context;
+
+public class PetContext : DbContext
+{
+    public DbSet<Identity> Identities { get; set; }
+    public DbSet<Pet> Pets { get; set; }
+}

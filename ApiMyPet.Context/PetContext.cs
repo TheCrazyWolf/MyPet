dcebadcc45ca8b742e37203a17b91e710a021e78
TheCrazyWolf <thecrazywolf63@gmail.com>
2025-01-26 11:30:33 +0400

    Таблица Session

--- ApiMyPet.Context/PetContext.cs
+++ ApiMyPet.Context/PetContext.cs
@@ -8,4 +8,5 @@ public class PetContext : DbContext
 {
     public DbSet<Identity> Identities { get; set; }
     public DbSet<Pet> Pets { get; set; }
+    public DbSet<Session> Sessions { get; set; }>
 }
